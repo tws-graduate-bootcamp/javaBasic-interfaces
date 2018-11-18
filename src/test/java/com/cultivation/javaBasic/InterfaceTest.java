71eb54cd3a8ae83cd7bd29b7608a56df25d7df57
Liu Xia <lxconan@gmail.com>
2018-11-18 17:49:07 +0800

    [liuxia] feat: initial commit.

---
+++ src/test/java/com/cultivation/javaBasic/InterfaceTest.java
@@ -0,0 +1,59 @@
+package com.cultivation.javaBasic;
+
+import com.cultivation.javaBasic.showYourIntelligence.NameImpl;
+import com.cultivation.javaBasic.util.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class InterfaceTest {
+
+    @Test
+    void should_support_default_method() {
+        InterfaceWithDefaultMethodImpl instance = new InterfaceWithDefaultMethodImpl();
+
+        // TODO: please modify the following code to pass the test
+        // <--start
+        final String expected = null;
+        // --end-->
+
+        assertEquals(expected, instance.tellMeTheTruthOfTheUniverse());
+    }
+
+    @Test
+    void should_choose_override_method() {
+        InterfaceWithOverrideDefaultImpl instance = new InterfaceWithOverrideDefaultImpl();
+
+        // TODO: please modify the following code to pass the test
+        // <--start
+        final String expected = null;
+        // --end-->
+
+        assertEquals(expected, instance.tellMeTheTruthOfTheUniverse());
+    }
+
+    @Test
+    void should_choose_override_method_continued() {
+        InterfaceExtendsInterfaceWithDefaultMethod instance = new InterfaceExtendsInterfaceWithDefaultMethodImpl();
+
+        // TODO: please modify the following code to pass the test
+        // <--start
+        final String expected = null;
+        // --end-->
+
+        assertEquals(expected, instance.tellMeTheTruthOfTheUniverse());
+    }
+
+    @Test
+    void should_resolve_ambiguity_by_yourself() {
+        NameImpl instance = new NameImpl();
+
+        String name = instance.getName();
+
+        assertEquals("Person", name);
+    }
+}
+
+/*
+ * - Can you clone an object without a default constructor?
+ */
---
+++ src/test/java/com/cultivation/javaBasic/showYourIntelligence/NameImpl.java
@@ -0,0 +1,15 @@
+package com.cultivation.javaBasic.showYourIntelligence;
+
+import com.cultivation.javaBasic.util.Person;
+import com.cultivation.javaBasic.util.WithName;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class NameImpl implements WithName, Person {
+    @Override
+    public String getName() {
+        // TODO: please modify the following code to pass the test
+        // <--start
+        throw new NotImplementedException();
+        // --end-->
+    }
+}
